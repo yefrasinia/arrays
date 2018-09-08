0ff20818f40574b938b74a9dff36dd311f01c14e
yefrasinia <yefrasinia@macbook-yefrasinia.local>
2018-09-08 21:25:37 +0300

    Initial Commit

---
+++ f/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  f
+//
+//  Created by yefrasinia on 08.09.2018.
+//  Copyright © 2018 жшар. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
